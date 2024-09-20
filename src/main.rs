3399acdd1da4a4fdb6563074a4db834538147be7
container1 <hans@d-jschlich.dvspla.de>
2024-09-20 12:02:33 +0200

    ToDo fix mid()

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::{thread, time::Duration};
 use std::io::Write;
 
-fn normal(mut len: usize, symbl: char){
+
+fn right(mut len: usize, symbl: char){
+    
     let mut load : String = String::new();
     len-= 5 + len.to_string().len();
     for i in 1..len {
@@ -12,6 +14,7 @@ fn normal(mut len: usize, symbl: char){
         print!(" ");
     }
     print!("[");
+    
     for _t in 1..( len.to_string().len() + 2 - i.to_string().len()){
         print!(" ");
     }
@@ -24,10 +27,70 @@ fn normal(mut len: usize, symbl: char){
     thread::sleep_ms(10 );
     }
 }
+
+
+
+fn left(mut len: usize, symbl: char){
+    
+    let mut load : String = String::new();
+    len-= 5 + len.to_string().len();
+    for i in 1..len {
+    load += &String::from(symbl);
+    
+    print!("[");
+    for _t in 1..( len.to_string().len() + 2 - i.to_string().len()){
+        print!(" ");
+    }
+    
+    print!("{}",i);
+    
+    
+    print!(" ]");
+    
+    print!("{}\r", load);
+    
+
+    
+    
+    std::io::stdout().flush();
+    thread::sleep_ms(10 );
+    }
+}
+fn mid(mut len: usize, symbl: char){
+    
+    let mut load : String = String::new();
+    len-= 5 + len.to_string().len();
+    for i in 1..len {
+    load += &String::from(symbl);
+    print!("\r{}", load);
+    
+    
+    print!("[");
+    for _t in 1..( len.to_string().len() + 2 - i.to_string().len()){
+        print!(" ");
+    }
+    
+    
+    print!("{}",i);
+    
+    
+    print!(" ]");
+    
+
+    for _t in i..len{
+        print!(" ");
+    }
+    std::io::stdout().flush();
+    thread::sleep_ms(10 );
+    }
+}
 fn main() {
     if let Some((w, h)) = term_size::dimensions() {
         println!("Width: {}\nHeight: {}", w, h);
-        normal(w, '#');
+        right(w, '#');
+        
+        mid(w,'%');
+        left(w,'=');
         } else {
         println!("Unable to get term size :(")
     }
