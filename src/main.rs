69f683910d6e96838bd9cec7f4c6018f9e9e76b9
container1 <hans@d-jschlich.dvspla.de>
2024-09-24 11:59:01 +0200

    i hate cursors

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,27 @@
 use std::{thread, time::Duration};
-use std::io::Write;
+use std::io::{self, Write, SeekFrom};
+use std::io::Cursor;
+use crossterm::{
+    ExecutableCommand, execute,
+    cursor::{DisableBlinking, EnableBlinking, MoveTo, MoveLeft, RestorePosition, SavePosition}
+};
+// doesnt work
+fn nbox(x: usize, len:usize, i: usize)-> io::Result<()>{
+    io::stdout()
+    .execute(MoveLeft(- x.try_into().unwrap()))?;
+    print!("[");
+    for _t in 1..( len.to_string().len() + 2 - i.to_string().len()){
+        print!(" ");
+    }
+    
+    
+    print!("{}",i);
+    
+    
+    print!(" ]");
+    Ok(())
 
+}
 
 fn right(mut len: usize, symbl: char){
     
@@ -56,7 +77,8 @@ fn left(mut len: usize, symbl: char){
     thread::sleep_ms(10 );
     }
 }
-fn mid(mut len: usize, symbl: char){
+
+fn moveable(mut len: usize, symbl: char){
     
     let mut load : String = String::new();
     len-= 5 + len.to_string().len();
@@ -84,13 +106,38 @@ fn mid(mut len: usize, symbl: char){
     thread::sleep_ms(10 );
     }
 }
+
+// doesnt work 
+fn mid( len: usize, symbl: char){
+    let mut flag = false;
+    let mut load : String = String::new();
+    load += &String::from(symbl);
+    let mut l2 = String::from(""); 
+    for i in 1..len {
+    load += &String::from(symbl);
+    
+    print!("\r{}", load);
+    
+    
+    
+    
+    
+
+    for _t in i..len{
+        print!(" ");
+    }
+    nbox(len/2,len,i);
+    std::io::stdout().flush();
+    thread::sleep_ms(100);
+    }
+}
 fn main() {
     if let Some((w, h)) = term_size::dimensions() {
         println!("Width: {}\nHeight: {}", w, h);
-        right(w, '#');
+        // right(w, '#');
         
         mid(w,'%');
-        left(w,'=');
+        // left(w,'=');
         } else {
         println!("Unable to get term size :(")
     }
